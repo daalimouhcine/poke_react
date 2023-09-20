c53aa2cf652fff2eff774448f9c44fca7acbe707
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-09-21 00:00:28 +0200

    adding the package to handle the display items while scrolling

--- src/features/pokemon/PokemonList.tsx
+++ src/features/pokemon/PokemonList.tsx
@@ -3,11 +3,11 @@ import { fetchPokemonStart, fetchMorePokemonStart } from "./PokemonSlice";
 import { useEffect } from "react";
 import PokemonCard from "../../components/PokemonCard";
 import SkeletonCard from "../../components/skeletonCard";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const PokemonList = () => {
   const dispatch = useDispatch();
   const pokemonData = useSelector((state: any) => state.pokemon.data);
-  const loading = useSelector((state: any) => state.pokemon.loading);
 
   useEffect(() => {
     // call action creator to fetch data from API
@@ -15,6 +15,7 @@ const PokemonList = () => {
   }, []);
 
   const handleLoadMore = () => {
+    console.log("call handleLoadMore");
     // call action creator to fetch more data from API
     dispatch(fetchMorePokemonStart());
   };
@@ -29,19 +30,20 @@ const PokemonList = () => {
       {/* Check if pokemonData is defined before accessing its properties */}
       {pokemonData ? (
         <div className='flex gap-5 flex-wrap justify-evenly p-5'>
-          {pokemonData.map((pokemon: any) => (
-            <PokemonCard key={pokemon.name} apiLink={pokemon.url} />
-          ))}
+          <InfiniteScroll
+            dataLength={pokemonData.length}
+            next={handleLoadMore}
+            hasMore={true}
+            loader={<SkeletonCard />}
+            className='flex gap-5 flex-wrap justify-evenly p-5'>
+            {pokemonData.map((pokemon: any) => (
+              <PokemonCard key={pokemon.name} apiLink={pokemon.url} />
+            ))}
+          </InfiniteScroll>
         </div>
       ) : (
         <div>No data available</div>
       )}
-      {/* <button
-        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-        onClick={handleLoadMore}>
-        Load More
-      </button> */}
-       {/* <SkeletonCard /> */}
     </div>
   );
 };
